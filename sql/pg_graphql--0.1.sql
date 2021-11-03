572cee51eebc65b981f0f9f3d55592d638c19e9d
Oliver Rice <github@oliverrice.com>
2021-11-03 04:53:54 -0500

    gql.enum_value table -> view

--- sql/pg_graphql--0.1.sql
+++ sql/pg_graphql--0.1.sql
@@ -494,14 +494,6 @@ create or replace view gql.relationship as
 
 
 
-create table gql.enum_value(
-    id integer generated always as identity primary key,
-    type_ text not null,
-    value text not null,
-    description text,
-    unique (type_, value)
-);
-
 create table gql.field (
     id integer generated always as identity primary key,
     parent_type text,
@@ -618,7 +610,55 @@ where
     and pg_catalog.has_type_privilege(current_user, t.oid, 'USAGE');
 
 
-
+create or replace view gql.enum_value as
+select
+    type_::text,
+    value::text,
+    description::text
+from (
+    values
+        ('__TypeKind', 'SCALAR', null::text),
+        ('__TypeKind', 'OBJECT', null),
+        ('__TypeKind', 'INTERFACE', null),
+        ('__TypeKind', 'UNION', null),
+        ('__TypeKind', 'ENUM', null),
+        ('__TypeKind', 'INPUT_OBJECT', null),
+        ('__TypeKind', 'LIST', null),
+        ('__TypeKind', 'NON_NULL', null),
+        ('__DirectiveLocation', 'QUERY', 'Location adjacent to a query operation.'),
+        ('__DirectiveLocation', 'MUTATION', 'Location adjacent to a mutation operation.'),
+        ('__DirectiveLocation', 'SUBSCRIPTION', 'Location adjacent to a subscription operation.'),
+        ('__DirectiveLocation', 'FIELD', 'Location adjacent to a field.'),
+        ('__DirectiveLocation', 'FRAGMENT_DEFINITION', 'Location adjacent to a fragment definition.'),
+        ('__DirectiveLocation', 'FRAGMENT_SPREAD', 'Location adjacent to a fragment spread.'),
+        ('__DirectiveLocation', 'INLINE_FRAGMENT', 'Location adjacent to an inline fragment.'),
+        ('__DirectiveLocation', 'VARIABLE_DEFINITION', 'Location adjacent to a variable definition.'),
+        ('__DirectiveLocation', 'SCHEMA', 'Location adjacent to a schema definition.'),
+        ('__DirectiveLocation', 'SCALAR', 'Location adjacent to a scalar definition.'),
+        ('__DirectiveLocation', 'OBJECT', 'Location adjacent to an object type definition.'),
+        ('__DirectiveLocation', 'FIELD_DEFINITION', 'Location adjacent to a field definition.'),
+        ('__DirectiveLocation', 'ARGUMENT_DEFINITION', 'Location adjacent to an argument definition.'),
+        ('__DirectiveLocation', 'INTERFACE', 'Location adjacent to an interface definition.'),
+        ('__DirectiveLocation', 'UNION', 'Location adjacent to a union definition.'),
+        ('__DirectiveLocation', 'ENUM', 'Location adjacent to an enum definition.'),
+        ('__DirectiveLocation', 'ENUM_VALUE', 'Location adjacent to an enum value definition.'),
+        ('__DirectiveLocation', 'INPUT_OBJECT', 'Location adjacent to an input object type definition.'),
+        ('__DirectiveLocation', 'INPUT_FIELD_DEFINITION', 'Location adjacent to an input object field definition.')
+) x(type_, value, description)
+union all
+select
+    gql.to_pascal_case(t.typname),
+    e.enumlabel as value,
+    null::text
+from
+    pg_type t
+    join pg_enum e
+        on t.oid = e.enumtypid
+    join pg_catalog.pg_namespace n
+        on n.oid = t.typnamespace
+where
+    n.nspname not in ('gql', 'information_schema', 'pg_catalog')
+    and pg_catalog.has_type_privilege(current_user, t.oid, 'USAGE');
 
 
 create function gql.sql_type_to_gql_type(sql_type text)
@@ -1373,10 +1413,6 @@ select
 $$;
 
 
-
-
-
-
 create or replace function gql."resolve_enumValues"(type_ text, ast jsonb)
     returns jsonb
     stable
@@ -1387,7 +1423,7 @@ as $$
             'description', value::text,
             'deprecationReason', null
         )
-        order by id asc)
+    )
     from
         gql.enum_value ev where ev.type_ = $1;
 $$;
