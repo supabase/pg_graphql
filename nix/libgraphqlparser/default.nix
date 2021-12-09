40f9f5861a838dd48f94dfc35df813ad15eba7e4
Oliver Rice <github@oliverrice.com>
2021-12-09 05:44:19 -0600

    unblock m1 builds

--- nix/libgraphqlparser/default.nix
+++ nix/libgraphqlparser/default.nix
@@ -7,6 +7,7 @@ stdenv.mkDerivation {
     url = "https://github.com/graphql/libgraphqlparser/archive/refs/tags/0.7.0.tar.gz";
     sha256 = "63dae018f970dc2bdce431cbafbfa0bd3e6b10bba078bb997a3c1a40894aa35c";
   };
-  inherit cmake; inherit python2;
+  buildInputs = [ python2 ];
+  inherit cmake;
 }
 
--- shell.nix
+++ shell.nix
@@ -1,8 +1,7 @@
 let
   nixpkgs = builtins.fetchTarball {
-    name = "2020-12-22";
-    url = "https://github.com/NixOS/nixpkgs/archive/2a058487cb7a50e7650f1657ee0151a19c59ec3b.tar.gz";
-    sha256 = "1h8c0mk6jlxdmjqch6ckj30pax3hqh6kwjlvp2021x3z4pdzrn9p";
+    name = "2021-12";
+    url = "https://github.com/NixOS/nixpkgs/archive/5f8babdd259d68ff8052dfc8d650ebdf9cc3bd75.tar.gz";
   };
 in with import nixpkgs {};
 mkShell {
@@ -14,5 +13,5 @@ mkShell {
       pg13WithExt = pgWithExt { pg = postgresql_13; };
       pg_w_pg_graphql = callPackage ./nix/pg_graphql/pgScript.nix { postgresql = pg13WithExt; };
     in
-    [ pg_w_pg_graphql nixops ];
+    [ pg_w_pg_graphql ];
 }
