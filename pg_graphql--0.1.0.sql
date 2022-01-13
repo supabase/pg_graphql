390262339b53f4c3339a5939a6e36ce01c2eaf5d
Oliver Rice <github@oliverrice.com>
2022-01-13 12:22:20 -0600

    remove unused to_pascal_case

--- pg_graphql--0.1.0.sql
+++ pg_graphql--0.1.0.sql
@@ -176,17 +176,6 @@ select
 from
     unnest(string_to_array($1, '_')) with ordinality x(part, part_ix)
 $$;
-create function graphql.to_pascal_case(text)
-    returns text
-    language sql
-    immutable
-as
-$$
-select
-    string_agg(initcap(part), '')
-from
-    unnest(string_to_array($1, '_')) x(part)
-$$;
 create or replace function graphql.alias_or_name_literal(field jsonb)
     returns text
     language sql
