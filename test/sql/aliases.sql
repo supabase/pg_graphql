119b613135a855762623d10f58f6a548d021d6e7
Oliver Rice <github@oliverrice.com>
2022-01-14 08:40:09 -0600

    enable aliasing top level data key (#41)
    * enable aliasing top level data key
    
    * test/aliases.sql fix test data setup

---
+++ test/sql/aliases.sql
@@ -0,0 +1,71 @@
+begin;
+
+    create table account(
+        id serial primary key,
+        email varchar(255) not null
+    );
+
+
+    insert into public.account(email)
+    values
+        ('aardvark@x.com');
+
+
+    create table blog(
+        id serial primary key,
+        owner_id integer not null references account(id),
+        name varchar(255) not null
+    );
+
+
+    insert into blog(owner_id, name)
+    values
+        (1, 'A: Blog 1');
+
+    -- Connection: alias all field types and operation
+    select jsonb_pretty(
+        graphql.resolve($$
+    {
+      aA: allAccounts(first: 1) {
+        tc: totalCount
+        pi: pageInfo {
+          hnp: hasNextPage
+        }
+        e: edges {
+          c: cursor
+          n: node{
+            id_: id
+            ni: nodeId
+            b: blogs {
+              tc2: totalCount
+            }
+          }
+        }
+      }
+    }
+        $$)
+    );
+
+
+    select graphql.resolve($$
+    {
+      acc: account(id: "WyJhY2NvdW50IiwgMV0=") {
+        id_: id
+        N: nodeId
+      }
+    }
+    $$);
+
+
+    select graphql.resolve($$
+    query Introspec {
+      s: __schema {
+        q: queryType {
+          n: name
+        }
+      }
+    }
+    $$);
+
+
+rollback;
