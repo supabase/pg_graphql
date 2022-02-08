eb9cb82a83d7d5498ef8be643130b71cceaba00c
Oliver Rice <github@oliverrice.com>
2022-02-08 06:19:02 -0600

    remove dangling types

--- pg_graphql--0.1.0.sql
+++ pg_graphql--0.1.0.sql
@@ -544,15 +544,12 @@ create type graphql.meta_kind as enum (
     'OrderBy',
     'FilterEntity',
     'UpsertNode',
-    'OnConflict',
 
 -- GraphQL Type Derived
     'FilterType',
 
 -- Enum Derived
-    'Enum',
-    'SelectableColumns',
-    'UpdatableColumns'
+    'Enum'
 );
 -- https://github.com/graphql/graphql-js/blob/main/src/type/introspection.ts#L197
 create type graphql.type_kind as enum (
@@ -629,9 +626,6 @@ as $$
             when rec.meta_kind='Connection'   then format('%sConnection', base_type_name)
             when rec.meta_kind='OrderBy'      then format('%sOrderBy',    base_type_name)
             when rec.meta_kind='FilterEntity' then format('%sFilter',     base_type_name)
-            when rec.meta_kind='OnConflict'   then format('%sOnConflict', base_type_name)
-            when rec.meta_kind='SelectableColumns' then format('%sSelectableField',      base_type_name)
-            when rec.meta_kind='UpdatableColumns'  then format('%sUpdatableField', base_type_name)
             when rec.meta_kind='FilterType'        then format('%sFilter',     graphql.type_name(rec.graphql_type_id))
             when rec.meta_kind='OrderByDirection'  then rec.meta_kind::text
             when rec.meta_kind='PageInfo'     then rec.meta_kind::text
@@ -880,10 +874,7 @@ begin
                     ('OBJECT',                    'Connection',              null,       ent.entity),
                     ('INPUT_OBJECT',              'OrderBy',                 null,       ent.entity),
                     ('INPUT_OBJECT',              'FilterEntity',            null,       ent.entity),
-                    ('INPUT_OBJECT',              'UpsertNode',              null,       ent.entity),
-                    ('INPUT_OBJECT',              'OnConflict',              null,       ent.entity),
-                    ('ENUM',                      'SelectableColumns',       null,       ent.entity),
-                    ('ENUM',                      'UpdatableColumns',        null,       ent.entity)
+                    ('INPUT_OBJECT',              'UpsertNode',              null,       ent.entity)
             ) x(type_kind, meta_kind, description, entity);
 
 
@@ -1729,110 +1720,55 @@ create view graphql.field as
         end;
 create view graphql.enum_value as
     select
-        type_,
-        value,
-        column_name,
-        description
+        type_::text,
+        value::text,
+        description::text
+    from (
+        values
+            ('__TypeKind', 'SCALAR', null::text),
+            ('__TypeKind', 'OBJECT', null),
+            ('__TypeKind', 'INTERFACE', null),
+            ('__TypeKind', 'UNION', null),
+            ('__TypeKind', 'ENUM', null),
+            ('__TypeKind', 'INPUT_OBJECT', null),
+            ('__TypeKind', 'LIST', null),
+            ('__TypeKind', 'NON_NULL', null),
+            ('__DirectiveLocation', 'QUERY', 'Location adjacent to a query operation.'),
+            ('__DirectiveLocation', 'MUTATION', 'Location adjacent to a mutation operation.'),
+            ('__DirectiveLocation', 'SUBSCRIPTION', 'Location adjacent to a subscription operation.'),
+            ('__DirectiveLocation', 'FIELD', 'Location adjacent to a field.'),
+            ('__DirectiveLocation', 'FRAGMENT_DEFINITION', 'Location adjacent to a fragment definition.'),
+            ('__DirectiveLocation', 'FRAGMENT_SPREAD', 'Location adjacent to a fragment spread.'),
+            ('__DirectiveLocation', 'INLINE_FRAGMENT', 'Location adjacent to an inline fragment.'),
+            ('__DirectiveLocation', 'VARIABLE_DEFINITION', 'Location adjacent to a variable definition.'),
+            ('__DirectiveLocation', 'SCHEMA', 'Location adjacent to a schema definition.'),
+            ('__DirectiveLocation', 'SCALAR', 'Location adjacent to a scalar definition.'),
+            ('__DirectiveLocation', 'OBJECT', 'Location adjacent to an object type definition.'),
+            ('__DirectiveLocation', 'FIELD_DEFINITION', 'Location adjacent to a field definition.'),
+            ('__DirectiveLocation', 'ARGUMENT_DEFINITION', 'Location adjacent to an argument definition.'),
+            ('__DirectiveLocation', 'INTERFACE', 'Location adjacent to an interface definition.'),
+            ('__DirectiveLocation', 'UNION', 'Location adjacent to a union definition.'),
+            ('__DirectiveLocation', 'ENUM', 'Location adjacent to an enum definition.'),
+            ('__DirectiveLocation', 'ENUM_VALUE', 'Location adjacent to an enum value definition.'),
+            ('__DirectiveLocation', 'INPUT_OBJECT', 'Location adjacent to an input object type definition.'),
+            ('__DirectiveLocation', 'INPUT_FIELD_DEFINITION', 'Location adjacent to an input object field definition.'),
+            -- pg_graphql Constant
+            ('OrderByDirection', 'AscNullsFirst', 'Ascending order, nulls first'),
+            ('OrderByDirection', 'AscNullsLast', 'Ascending order, nulls last'),
+            ('OrderByDirection', 'DescNullsFirst', 'Descending order, nulls first'),
+            ('OrderByDirection', 'DescNullsLast', 'Descending order, nulls last')
+    ) x(type_, value, description)
+    union all
+    select
+        ty.name,
+        e.enumlabel as value,
+        null::text
     from
-        (
-            select
-                type_::text,
-                value::text,
-                null::text as column_name,
-                0 as column_attribute_num,
-                description::text
-            from (
-                values
-                    ('__TypeKind', 'SCALAR', null::text),
-                    ('__TypeKind', 'OBJECT', null),
-                    ('__TypeKind', 'INTERFACE', null),
-                    ('__TypeKind', 'UNION', null),
-                    ('__TypeKind', 'ENUM', null),
-                    ('__TypeKind', 'INPUT_OBJECT', null),
-                    ('__TypeKind', 'LIST', null),
-                    ('__TypeKind', 'NON_NULL', null),
-                    ('__DirectiveLocation', 'QUERY', 'Location adjacent to a query operation.'),
-                    ('__DirectiveLocation', 'MUTATION', 'Location adjacent to a mutation operation.'),
-                    ('__DirectiveLocation', 'SUBSCRIPTION', 'Location adjacent to a subscription operation.'),
-                    ('__DirectiveLocation', 'FIELD', 'Location adjacent to a field.'),
-                    ('__DirectiveLocation', 'FRAGMENT_DEFINITION', 'Location adjacent to a fragment definition.'),
-                    ('__DirectiveLocation', 'FRAGMENT_SPREAD', 'Location adjacent to a fragment spread.'),
-                    ('__DirectiveLocation', 'INLINE_FRAGMENT', 'Location adjacent to an inline fragment.'),
-                    ('__DirectiveLocation', 'VARIABLE_DEFINITION', 'Location adjacent to a variable definition.'),
-                    ('__DirectiveLocation', 'SCHEMA', 'Location adjacent to a schema definition.'),
-                    ('__DirectiveLocation', 'SCALAR', 'Location adjacent to a scalar definition.'),
-                    ('__DirectiveLocation', 'OBJECT', 'Location adjacent to an object type definition.'),
-                    ('__DirectiveLocation', 'FIELD_DEFINITION', 'Location adjacent to a field definition.'),
-                    ('__DirectiveLocation', 'ARGUMENT_DEFINITION', 'Location adjacent to an argument definition.'),
-                    ('__DirectiveLocation', 'INTERFACE', 'Location adjacent to an interface definition.'),
-                    ('__DirectiveLocation', 'UNION', 'Location adjacent to a union definition.'),
-                    ('__DirectiveLocation', 'ENUM', 'Location adjacent to an enum definition.'),
-                    ('__DirectiveLocation', 'ENUM_VALUE', 'Location adjacent to an enum value definition.'),
-                    ('__DirectiveLocation', 'INPUT_OBJECT', 'Location adjacent to an input object type definition.'),
-                    ('__DirectiveLocation', 'INPUT_FIELD_DEFINITION', 'Location adjacent to an input object field definition.'),
-                    -- pg_graphql Constant
-                    ('OrderByDirection', 'AscNullsFirst', 'Ascending order, nulls first'),
-                    ('OrderByDirection', 'AscNullsLast', 'Ascending order, nulls last'),
-                    ('OrderByDirection', 'DescNullsFirst', 'Descending order, nulls first'),
-                    ('OrderByDirection', 'DescNullsLast', 'Descending order, nulls last')
-            ) x(type_, value, description)
-            union all
-            select
-                ty.name,
-                e.enumlabel as value,
-                null::text,
-                0,
-                null::text
-            from
-                graphql.type ty
-                join pg_enum e
-                    on ty.enum = e.enumtypid
-            where
-                ty.enum is not null
-            union all
-            select
-                gt.name,
-                graphql.field_name_for_column(ec.entity, ec.column_name),
-                ec.column_name,
-                ec.column_attribute_num,
-                null::text
-            from
-                graphql.type gt
-                join graphql.entity_column ec
-                    on gt.entity = ec.entity
-            where
-                gt.meta_kind = 'SelectableColumns'
-                and pg_catalog.has_column_privilege(
-                    current_user,
-                    gt.entity,
-                    ec.column_name,
-                    'SELECT'
-                )
-            union all
-            select
-                gt.name,
-                graphql.field_name_for_column(ec.entity, ec.column_name),
-                ec.column_name,
-                ec.column_attribute_num,
-                null::text
-            from
-                graphql.type gt
-                join graphql.entity_column ec
-                    on gt.entity = ec.entity
-            where
-                gt.meta_kind = 'UpdatableColumns'
-                and pg_catalog.has_column_privilege(
-                    current_user,
-                    gt.entity,
-                    ec.column_name,
-                    'UPDATE'
-                )
-        ) x
-    order by
-        type_,
-        column_attribute_num,
-        value,
-        description;
+        graphql.type ty
+        join pg_enum e
+            on ty.enum = e.enumtypid
+    where
+        ty.enum is not null;
 create or replace function graphql.arg_index(arg_name text, variable_definitions jsonb)
     returns int
     immutable
@@ -2772,14 +2708,6 @@ begin
     return result;
 end;
 $$;
-
-/*
-where
-            case
-                when (select count(deleted.res) > %s from deleted) then graphql.exception($a$delete impacts too many records$a$)::boolean
-                else true
-            end
-*/
 create or replace function graphql.build_insert(
     ast jsonb,
     variable_definitions jsonb = '[]',
--- src/sql/reflection/type/enum_value.sql
+++ src/sql/reflection/type/enum_value.sql
@@ -1,106 +1,51 @@
 create view graphql.enum_value as
     select
-        type_,
-        value,
-        column_name,
-        description
+        type_::text,
+        value::text,
+        description::text
+    from (
+        values
+            ('__TypeKind', 'SCALAR', null::text),
+            ('__TypeKind', 'OBJECT', null),
+            ('__TypeKind', 'INTERFACE', null),
+            ('__TypeKind', 'UNION', null),
+            ('__TypeKind', 'ENUM', null),
+            ('__TypeKind', 'INPUT_OBJECT', null),
+            ('__TypeKind', 'LIST', null),
+            ('__TypeKind', 'NON_NULL', null),
+            ('__DirectiveLocation', 'QUERY', 'Location adjacent to a query operation.'),
+            ('__DirectiveLocation', 'MUTATION', 'Location adjacent to a mutation operation.'),
+            ('__DirectiveLocation', 'SUBSCRIPTION', 'Location adjacent to a subscription operation.'),
+            ('__DirectiveLocation', 'FIELD', 'Location adjacent to a field.'),
+            ('__DirectiveLocation', 'FRAGMENT_DEFINITION', 'Location adjacent to a fragment definition.'),
+            ('__DirectiveLocation', 'FRAGMENT_SPREAD', 'Location adjacent to a fragment spread.'),
+            ('__DirectiveLocation', 'INLINE_FRAGMENT', 'Location adjacent to an inline fragment.'),
+            ('__DirectiveLocation', 'VARIABLE_DEFINITION', 'Location adjacent to a variable definition.'),
+            ('__DirectiveLocation', 'SCHEMA', 'Location adjacent to a schema definition.'),
+            ('__DirectiveLocation', 'SCALAR', 'Location adjacent to a scalar definition.'),
+            ('__DirectiveLocation', 'OBJECT', 'Location adjacent to an object type definition.'),
+            ('__DirectiveLocation', 'FIELD_DEFINITION', 'Location adjacent to a field definition.'),
+            ('__DirectiveLocation', 'ARGUMENT_DEFINITION', 'Location adjacent to an argument definition.'),
+            ('__DirectiveLocation', 'INTERFACE', 'Location adjacent to an interface definition.'),
+            ('__DirectiveLocation', 'UNION', 'Location adjacent to a union definition.'),
+            ('__DirectiveLocation', 'ENUM', 'Location adjacent to an enum definition.'),
+            ('__DirectiveLocation', 'ENUM_VALUE', 'Location adjacent to an enum value definition.'),
+            ('__DirectiveLocation', 'INPUT_OBJECT', 'Location adjacent to an input object type definition.'),
+            ('__DirectiveLocation', 'INPUT_FIELD_DEFINITION', 'Location adjacent to an input object field definition.'),
+            -- pg_graphql Constant
+            ('OrderByDirection', 'AscNullsFirst', 'Ascending order, nulls first'),
+            ('OrderByDirection', 'AscNullsLast', 'Ascending order, nulls last'),
+            ('OrderByDirection', 'DescNullsFirst', 'Descending order, nulls first'),
+            ('OrderByDirection', 'DescNullsLast', 'Descending order, nulls last')
+    ) x(type_, value, description)
+    union all
+    select
+        ty.name,
+        e.enumlabel as value,
+        null::text
     from
-        (
-            select
-                type_::text,
-                value::text,
-                null::text as column_name,
-                0 as column_attribute_num,
-                description::text
-            from (
-                values
-                    ('__TypeKind', 'SCALAR', null::text),
-                    ('__TypeKind', 'OBJECT', null),
-                    ('__TypeKind', 'INTERFACE', null),
-                    ('__TypeKind', 'UNION', null),
-                    ('__TypeKind', 'ENUM', null),
-                    ('__TypeKind', 'INPUT_OBJECT', null),
-                    ('__TypeKind', 'LIST', null),
-                    ('__TypeKind', 'NON_NULL', null),
-                    ('__DirectiveLocation', 'QUERY', 'Location adjacent to a query operation.'),
-                    ('__DirectiveLocation', 'MUTATION', 'Location adjacent to a mutation operation.'),
-                    ('__DirectiveLocation', 'SUBSCRIPTION', 'Location adjacent to a subscription operation.'),
-                    ('__DirectiveLocation', 'FIELD', 'Location adjacent to a field.'),
-                    ('__DirectiveLocation', 'FRAGMENT_DEFINITION', 'Location adjacent to a fragment definition.'),
-                    ('__DirectiveLocation', 'FRAGMENT_SPREAD', 'Location adjacent to a fragment spread.'),
-                    ('__DirectiveLocation', 'INLINE_FRAGMENT', 'Location adjacent to an inline fragment.'),
-                    ('__DirectiveLocation', 'VARIABLE_DEFINITION', 'Location adjacent to a variable definition.'),
-                    ('__DirectiveLocation', 'SCHEMA', 'Location adjacent to a schema definition.'),
-                    ('__DirectiveLocation', 'SCALAR', 'Location adjacent to a scalar definition.'),
-                    ('__DirectiveLocation', 'OBJECT', 'Location adjacent to an object type definition.'),
-                    ('__DirectiveLocation', 'FIELD_DEFINITION', 'Location adjacent to a field definition.'),
-                    ('__DirectiveLocation', 'ARGUMENT_DEFINITION', 'Location adjacent to an argument definition.'),
-                    ('__DirectiveLocation', 'INTERFACE', 'Location adjacent to an interface definition.'),
-                    ('__DirectiveLocation', 'UNION', 'Location adjacent to a union definition.'),
-                    ('__DirectiveLocation', 'ENUM', 'Location adjacent to an enum definition.'),
-                    ('__DirectiveLocation', 'ENUM_VALUE', 'Location adjacent to an enum value definition.'),
-                    ('__DirectiveLocation', 'INPUT_OBJECT', 'Location adjacent to an input object type definition.'),
-                    ('__DirectiveLocation', 'INPUT_FIELD_DEFINITION', 'Location adjacent to an input object field definition.'),
-                    -- pg_graphql Constant
-                    ('OrderByDirection', 'AscNullsFirst', 'Ascending order, nulls first'),
-                    ('OrderByDirection', 'AscNullsLast', 'Ascending order, nulls last'),
-                    ('OrderByDirection', 'DescNullsFirst', 'Descending order, nulls first'),
-                    ('OrderByDirection', 'DescNullsLast', 'Descending order, nulls last')
-            ) x(type_, value, description)
-            union all
-            select
-                ty.name,
-                e.enumlabel as value,
-                null::text,
-                0,
-                null::text
-            from
-                graphql.type ty
-                join pg_enum e
-                    on ty.enum = e.enumtypid
-            where
-                ty.enum is not null
-            union all
-            select
-                gt.name,
-                graphql.field_name_for_column(ec.entity, ec.column_name),
-                ec.column_name,
-                ec.column_attribute_num,
-                null::text
-            from
-                graphql.type gt
-                join graphql.entity_column ec
-                    on gt.entity = ec.entity
-            where
-                gt.meta_kind = 'SelectableColumns'
-                and pg_catalog.has_column_privilege(
-                    current_user,
-                    gt.entity,
-                    ec.column_name,
-                    'SELECT'
-                )
-            union all
-            select
-                gt.name,
-                graphql.field_name_for_column(ec.entity, ec.column_name),
-                ec.column_name,
-                ec.column_attribute_num,
-                null::text
-            from
-                graphql.type gt
-                join graphql.entity_column ec
-                    on gt.entity = ec.entity
-            where
-                gt.meta_kind = 'UpdatableColumns'
-                and pg_catalog.has_column_privilege(
-                    current_user,
-                    gt.entity,
-                    ec.column_name,
-                    'UPDATE'
-                )
-        ) x
-    order by
-        type_,
-        column_attribute_num,
-        value,
-        description;
+        graphql.type ty
+        join pg_enum e
+            on ty.enum = e.enumtypid
+    where
+        ty.enum is not null;
