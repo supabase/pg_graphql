82e0a3f12d08370e5d5ba2e4b3b4dfde63b3696d
Oliver Rice <github@oliverrice.com>
2022-02-16 12:13:50 -0600

    filter types by visibility in search path

---
+++ test/sql/search_path_filters_visibility.sql
@@ -0,0 +1,34 @@
+begin;
+
+    create table public.foo(
+        id serial primary key
+    );
+
+    create schema other;
+
+    create table other.bar(
+        id serial primary key
+    );
+
+    -- public.foo is visible, other.bar is not
+    select
+        name
+    from
+        graphql.type
+    where
+        entity is not null
+        and meta_kind = 'Node';
+
+
+    -- switch search path to other
+    set search_path = other;
+    -- other.bar is visible, public.foo is not is not
+    select
+        name
+    from
+        graphql.type
+    where
+        entity is not null
+        and meta_kind = 'Node';
+
+rollback;
