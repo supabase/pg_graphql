3440f21115aafa8a294c6df3da21cc92f179d288
Oliver Rice <github@oliverrice.com>
2022-04-04 11:05:16 -0500

    handle inclusion and null-ing of top "data" and "error" keys correctly

--- pg_graphql--0.1.5.sql
+++ pg_graphql--0.1.5.sql
@@ -4535,9 +4535,9 @@ declare
     q text;
     data_ jsonb;
     request_data jsonb;
-    errors_ text[] = case
-        when parsed.error is not null then array[parsed.error]
-        when ast_operation is null then array['unknown operation']
+    errors_ jsonb[] = case
+        when parsed.error is not null then array[jsonb_build_object('message', parsed.error)]
+        when ast_operation is null then array[jsonb_build_object('message', 'unknown operation')]
         else '{}'
     end;
 
@@ -4560,162 +4560,175 @@ declare
     error_message text;
 begin
 
+
+    if errors_ <> '{}' then
+       -- If an error was encountered before execution begins, the data entry should not be present in the result.
+       return jsonb_build_object(
+        'errors', to_jsonb(errors_)
+       );
+    end if;
+
     begin
 
         -- Build query if not in cache
-        if errors_ = '{}' then
+        for statement_ix in 0..(n_statements - 1) loop
 
-            for statement_ix in 0..(n_statements - 1) loop
+            ast_statement = (
+                 ast_operation -> 'selectionSet' -> 'selections' -> statement_ix
+            );
 
-                ast_statement = (
-                     ast_operation -> 'selectionSet' -> 'selections' -> statement_ix
-                );
+            prepared_statement_name = (
+                case
+                    when operation = 'Query' then graphql.cache_key(current_user::regrole, ast_statement, variables)
+                    -- If not a query (mutation) don't attempt to cache
+                    else md5(format('%s%s%s',random(),random(),random()))
+                end
+            );
 
-                prepared_statement_name = (
-                    case
-                        when operation = 'Query' then graphql.cache_key(current_user::regrole, ast_statement, variables)
-                        -- If not a query (mutation) don't attempt to cache
-                        else md5(format('%s%s%s',random(),random(),random()))
-                    end
-                );
+            if errors_ = '{}' and not graphql.prepared_statement_exists(prepared_statement_name) then
 
-                if errors_ = '{}' and not graphql.prepared_statement_exists(prepared_statement_name) then
+                    ast_locless = graphql.ast_pass_strip_loc(ast_statement);
+                    fragment_definitions = graphql.ast_pass_strip_loc(
+                        jsonb_path_query_array(ast, '$.definitions[*] ? (@.kind == "FragmentDefinition")')
+                    );
+                    -- Skip fragment inline when no fragments are present
+                    ast_inlined = case
+                        when fragment_definitions = '[]'::jsonb then ast_locless
+                        else graphql.ast_pass_fragments(ast_locless, fragment_definitions)
+                    end;
 
-                        ast_locless = graphql.ast_pass_strip_loc(ast_statement);
-                        fragment_definitions = graphql.ast_pass_strip_loc(
-                            jsonb_path_query_array(ast, '$.definitions[*] ? (@.kind == "FragmentDefinition")')
+                    field_meta_kind = f.meta_kind
+                        from
+                            graphql.field f
+                        where
+                            f.parent_type = operation::text
+                            and f.name = graphql.name_literal(ast_inlined);
+
+                    if field_meta_kind is null then
+                        perform graphql.exception_unknown_field(
+                            graphql.name_literal(ast_inlined),
+                            operation::text
                         );
-                        -- Skip fragment inline when no fragments are present
-                        ast_inlined = case
-                            when fragment_definitions = '[]'::jsonb then ast_locless
-                            else graphql.ast_pass_fragments(ast_locless, fragment_definitions)
-                        end;
+                    end if;
 
-                        field_meta_kind = f.meta_kind
+                    q = case field_meta_kind
+                        when 'Mutation.insert' then
+                            graphql.build_insert(
+                                ast := ast_inlined,
+                                variable_definitions := variable_definitions,
+                                variables := variables
+                            )
+                        when 'Mutation.delete' then
+                            graphql.build_delete(
+                                ast := ast_inlined,
+                                variable_definitions := variable_definitions,
+                                variables := variables
+                            )
+                        when 'Mutation.update' then
+                            graphql.build_update(
+                                ast := ast_inlined,
+                                variable_definitions := variable_definitions,
+                                variables := variables
+                            )
+                        when 'Query.collection' then
+                                graphql.build_connection_query(
+                                    ast := ast_inlined,
+                                    variable_definitions := variable_definitions,
+                                    variables := variables,
+                                    parent_type :=  'Query',
+                                    parent_block_name := null
+                                )
+                        when 'Query.heartbeat' then graphql.build_heartbeat_query(ast_inlined)
+                    end;
+
+                    if q is null and operation = 'Query' then
+
+                        meta_kind = type_.meta_kind
                             from
-                                graphql.field f
+                                graphql.field
+                                join graphql.type type_
+                                    on field.type_ = type_.name
                             where
-                                f.parent_type = operation::text
-                                and f.name = graphql.name_literal(ast_inlined);
+                                field.parent_type = 'Query'
+                                and field.name = graphql.name_literal(ast_inlined);
 
-                        if field_meta_kind is null then
+                        if meta_kind is null then
                             perform graphql.exception_unknown_field(
                                 graphql.name_literal(ast_inlined),
-                                operation::text
+                                'Query'
                             );
                         end if;
 
-                        q = case field_meta_kind
-                            when 'Mutation.insert' then
-                                graphql.build_insert(
+                        data_ = case meta_kind
+                            when '__Schema' then
+                                graphql."resolve___Schema"(
                                     ast := ast_inlined,
-                                    variable_definitions := variable_definitions,
-                                    variables := variables
+                                    variable_definitions := variable_definitions
                                 )
-                            when 'Mutation.delete' then
-                                graphql.build_delete(
-                                    ast := ast_inlined,
-                                    variable_definitions := variable_definitions,
-                                    variables := variables
-                                )
-                            when 'Mutation.update' then
-                                graphql.build_update(
-                                    ast := ast_inlined,
-                                    variable_definitions := variable_definitions,
-                                    variables := variables
-                                )
-                            when 'Query.collection' then
-                                    graphql.build_connection_query(
-                                        ast := ast_inlined,
-                                        variable_definitions := variable_definitions,
-                                        variables := variables,
-                                        parent_type :=  'Query',
-                                        parent_block_name := null
-                                    )
-                            when 'Query.heartbeat' then graphql.build_heartbeat_query(ast_inlined)
-                        end;
-
-                        if q is null and operation = 'Query' then
-
-                            meta_kind = type_.meta_kind
-                                from
-                                    graphql.field
-                                    join graphql.type type_
-                                        on field.type_ = type_.name
-                                where
-                                    field.parent_type = 'Query'
-                                    and field.name = graphql.name_literal(ast_inlined);
-
-                            if meta_kind is null then
-                                perform graphql.exception_unknown_field(
+                            when '__Type' then
+                                jsonb_build_object(
                                     graphql.name_literal(ast_inlined),
-                                    'Query'
-                                );
-                            end if;
-
-                            data_ = case meta_kind
-                                when '__Schema' then
-                                    graphql."resolve___Schema"(
-                                        ast := ast_inlined,
-                                        variable_definitions := variable_definitions
+                                    graphql."resolve___Type"(
+                                        (
+                                            select
+                                                name
+                                            from
+                                                graphql.type type_
+                                            where
+                                                name = graphql.argument_value_by_name('name', ast_inlined)
+                                        ),
+                                        ast_inlined
                                     )
-                                when '__Type' then
-                                    jsonb_build_object(
-                                        graphql.name_literal(ast_inlined),
-                                        graphql."resolve___Type"(
-                                            (
-                                                select
-                                                    name
-                                                from
-                                                    graphql.type type_
-                                                where
-                                                    name = graphql.argument_value_by_name('name', ast_inlined)
-                                            ),
-                                            ast_inlined
-                                        )
-                                    )
-                                else null::jsonb
-                            end;
-                        end if;
-                end if;
+                                )
+                            else null::jsonb
+                        end;
+                    end if;
+            end if;
 
-                if errors_ = '{}' and q is not null then
-                    execute graphql.prepared_statement_create_clause(prepared_statement_name, variable_definitions, q);
-                end if;
+            if errors_ = '{}' and q is not null then
+                execute graphql.prepared_statement_create_clause(prepared_statement_name, variable_definitions, q);
+            end if;
 
-                if errors_ = '{}' and data_ is null then
-                    -- Call prepared statement respecting passed values and variable definition defaults
-                    execute graphql.prepared_statement_execute_clause(prepared_statement_name, variable_definitions, variables) into data_;
-                    data_ = jsonb_build_object(
-                        graphql.alias_or_name_literal(ast_statement),
-                        data_
-                    );
-                end if;
+            if errors_ = '{}' and data_ is null then
+                -- Call prepared statement respecting passed values and variable definition defaults
+                execute graphql.prepared_statement_execute_clause(prepared_statement_name, variable_definitions, variables) into data_;
+                data_ = jsonb_build_object(
+                    graphql.alias_or_name_literal(ast_statement),
+                    data_
+                );
+            end if;
 
-                -- Add data to final state
-                request_data = case
-                    when request_data is null then data_
-                    else request_data || data_
-                end;
+            -- Add data to final state
+            request_data = case
+                when request_data is null then data_
+                else request_data || data_
+            end;
 
-                -- reset loop vars
-                q = null;
-                data_ = null;
+            -- reset loop vars
+            q = null;
+            data_ = null;
 
-            end loop;
-        end if;
+        end loop;
 
     exception when others then
         get stacked diagnostics error_message = MESSAGE_TEXT;
-        errors_ = errors_ || error_message;
+        errors_ = errors_ || jsonb_build_object('message', error_message);
         -- Do no show partial or rolled back results
         request_data = null;
     end;
 
 
-    return jsonb_build_object(
-        'data', request_data,
-        'errors', to_jsonb(errors_)
+    return (
+        -- If no errors were encountered during the requested operation, the errors entry should not be present in the result.
+        jsonb_build_object('data', request_data)
+        || case
+           when errors_ <> '{}' then jsonb_build_object(
+                'errors', to_jsonb(errors_),
+                -- If an error was encountered during the execution that prevented a valid response, the data entry in the response should be null
+                'data', null::text
+            )
+            else  '{}'
+        end
     );
 end
 $$;
