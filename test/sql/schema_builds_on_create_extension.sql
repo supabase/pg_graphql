c0b6db7761ca9e7f57d6d4fd02bb17b7beb1cdcb
Oliver Rice <github@oliverrice.com>
2022-04-26 12:08:52 -0500

    safedelete bugfix

--- test/sql/schema_builds_on_create_extension.sql
+++ test/sql/schema_builds_on_create_extension.sql
@@ -1,8 +1,4 @@
 drop extension pg_graphql;
 create extension pg_graphql;
 
-select jsonb_pretty(
-    graphql.resolve($$
-    { heartbeat }
-    $$)
-);
+select graphql.resolve($${ heartbeat }$$) -> 'data' ->> 'heartbeat' like '2%';
