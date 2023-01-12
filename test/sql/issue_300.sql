8d2ff141b53b2b0b010b787d0c6246063c72c750
Oliver Rice <github@oliverrice.com>
2023-01-12 16:41:25 -0600

    add failing test for issue 300

---
+++ test/sql/issue_300.sql
@@ -0,0 +1,42 @@
+begin;
+    -- https://github.com/supabase/pg_graphql/issues/300
+    create role api;
+
+    create table project (
+        id serial primary key,
+        title text not null,
+        created_at timestamptz not null default now(),
+        updated_at timestamptz not null default now()
+    );
+
+    grant usage on schema graphql to api;
+
+    revoke all on table project from api;
+    grant select on table project to api;
+    grant insert (id, title) on table project to api;
+    grant update (title) on table project to api;
+    grant delete on table project to api;
+
+    set role to 'api';
+
+    select jsonb_pretty(
+        graphql.resolve($$
+
+    mutation CreateProject {
+      insertIntoProjectCollection(objects: [
+        {title: "foo"}
+      ]) {
+        affectedCount
+        records {
+          id
+          title
+          createdAt
+          updatedAt
+        }
+      }
+    }
+    $$
+        )
+    );
+
+rollback;
