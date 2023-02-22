4b9383bf0a28bf9d8d50ac8d8322846fb1a43149
Oliver Rice <github@oliverrice.com>
2023-02-22 12:09:42 -0600

    added test case for negative max_rows comment directive / test formatting

--- test/sql/test_error_invalid_offset.sql
+++ test/sql/test_error_invalid_offset.sql
@@ -1,20 +1,20 @@
 begin;
 
-create table blog(
-                     id serial primary key,
-                     owner_id integer not null,
-                     name varchar(255) not null,
-                     description text
-);
+    create table blog(
+        id serial primary key,
+        owner_id integer not null,
+        name varchar(255) not null,
+        description text
+    );
 
-insert into blog(owner_id, name, description)
-values
-    (1, 'A: Blog 1', 'first'),
-    (2, 'A: Blog 2', 'second');
+    insert into blog(owner_id, name, description)
+    values
+        (1, 'A: Blog 1', 'first'),
+        (2, 'A: Blog 2', 'second');
 
 
-select graphql.resolve($$
-    {
+    select graphql.resolve($$
+        {
           blogCollection(first: -1) {
             edges {
               cursor
@@ -27,9 +27,25 @@ select graphql.resolve($$
 
     $$);
 
-select graphql.resolve($$
-    {
+    select graphql.resolve($$
+        {
           blogCollection(last: -1) {
+            edges {
+              cursor
+              node {
+                ownerId
+              }
+            }
+          }
+        }
+
+    $$);
+
+    comment on schema public is E'@graphql({"max_rows": -1, "inflect_names": true})';
+
+    select graphql.resolve($$
+        {
+          blogCollection {
             edges {
               cursor
               node {
