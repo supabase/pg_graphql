a84f1dec0a0ca2ab75be0f2b1aea74fd0d60804a
Raminder Singh <romi_ssk@yahoo.co.in>
2023-07-27 16:51:52 +0530

    add another test

--- test/sql/function_calls.sql
+++ test/sql/function_calls.sql
@@ -1,6 +1,17 @@
 begin;
 
     -- Only volatilve functions appear on the mutation object
+
+    create function add_smallints(a smallint, b smallint)
+        returns smallint language sql volatile
+    as $$ select a + b; $$;
+
+    select jsonb_pretty(graphql.resolve($$
+        mutation {
+            addSmallints(a: 1, b: 2)
+        }
+    $$));
+
     create function add_ints(a int, b int)
         returns int language sql volatile
     as $$ select a + b; $$;
