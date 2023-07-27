c7f6eaeaf5013fc3818b2742edd16760fba94ae5
Raminder Singh <romi_ssk@yahoo.co.in>
2023-07-27 16:48:15 +0530

    make volatile explicit

--- test/sql/function_calls.sql
+++ test/sql/function_calls.sql
@@ -1,7 +1,8 @@
 begin;
 
+    -- Only volatilve functions appear on the mutation object
     create function add_ints(a int, b int)
-        returns int language sql
+        returns int language sql volatile
     as $$ select a + b; $$;
 
     select jsonb_pretty(graphql.resolve($$
