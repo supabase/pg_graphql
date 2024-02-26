7312565aaaa462f020c2b3b9516c78cacb7797c6
Raminder Singh <romi_ssk@yahoo.co.in>
2024-02-26 11:41:53 +0530

    add more tests

--- test/sql/issue_237_field_merging.sql
+++ test/sql/issue_237_field_merging.sql
@@ -1,5 +1,6 @@
 begin;
     -- https://github.com/supabase/pg_graphql/issues/237
+    savepoint a;
     create table blog_post(
         id int primary key,
         a text,
@@ -47,4 +48,111 @@ begin;
         }
       $$)
     );
+
+    rollback to savepoint a;
+
+    create table account(
+        id serial primary key,
+        email varchar(255) not null
+    );
+
+    insert into public.account(email)
+    values
+        ('aardvark@x.com');
+
+    create table blog(
+        id serial primary key,
+        owner_id integer not null references account(id),
+        name varchar(255) not null
+    );
+
+    insert into blog(owner_id, name)
+    values
+        (1, 'A: Blog 1');
+
+    select jsonb_pretty(graphql.resolve($$ {
+        accountCollection {
+            edges {
+                node {
+                    email
+                    email
+                    id_alias: id
+                    id_alias: id
+                }
+            }
+        }
+    }$$));
+
+    select jsonb_pretty(graphql.resolve($$ {
+        accountCollection(first: 1) {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+        accountCollection(first: 1) {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+    }$$));
+
+    select jsonb_pretty(graphql.resolve($$ {
+        accountCollection(first: $count) {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+        accountCollection(first: $count) {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+    }$$,
+    jsonb_build_object(
+        'count', 1
+    )));
+
+    select jsonb_pretty(graphql.resolve($$ {
+        accountCollection {
+            edges {
+                cursor
+                cursor
+                node {
+                    id
+                    email
+                }
+                node {
+                    id
+                    email
+                }
+            }
+            edges {
+                cursor
+                cursor
+                node {
+                    id
+                    email
+                }
+                node {
+                    id
+                    email
+                }
+            }
+        }
+    }
+
+    $$));
+
 rollback;
--- test/sql/issue_237_field_merging_mismatched.sql
+++ test/sql/issue_237_field_merging_mismatched.sql
@@ -1,5 +1,6 @@
 begin;
     -- https://github.com/supabase/pg_graphql/issues/237
+    savepoint a;
     create table blog_post(
         id int primary key,
         a text,
@@ -72,4 +73,120 @@ begin;
         }
       $$)
     );
+
+    rollback to savepoint a;
+
+    create table account(
+        id serial primary key,
+        email varchar(255) not null
+    );
+
+    insert into public.account(email)
+    values
+        ('aardvark@x.com');
+
+    create table blog(
+        id serial primary key,
+        owner_id integer not null references account(id),
+        name varchar(255) not null
+    );
+
+    insert into blog(owner_id, name)
+    values
+        (1, 'A: Blog 1');
+
+    select jsonb_pretty(graphql.resolve($$ {
+        accountCollection {
+            edges {
+                node {
+                    email: id
+                    email
+                }
+            }
+        }
+    }$$));
+
+    select jsonb_pretty(graphql.resolve($$ {
+        accountCollection(first: 1) {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+        accountCollection(first: 2) {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+    }$$));
+
+    select jsonb_pretty(graphql.resolve($$ {
+        accountCollection(first: $count) {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+        accountCollection(first: 1) {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+    }$$,
+    jsonb_build_object(
+        'count', 1
+    )));
+
+    select jsonb_pretty(graphql.resolve($$ {
+        accountCollection(first: $count) {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+        accountCollection(first: $num) {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+    }$$,
+    jsonb_build_object(
+        'count', 1,
+        'num', 1
+    )));
+
+    select jsonb_pretty(graphql.resolve($$ {
+        accountCollection(first: 1) {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+        accountCollection {
+            edges {
+                node {
+                    id
+                    email
+                }
+            }
+        }
+    }$$));
+
 rollback;
