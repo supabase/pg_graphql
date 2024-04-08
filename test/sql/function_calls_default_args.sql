eed550e6e3f81a188c2b8fea5d592798c8bc52b8
Raminder Singh <romi_ssk@yahoo.co.in>
2024-04-08 16:02:35 +0530

    extend default null args tests with more types

--- test/sql/function_calls_default_args.sql
+++ test/sql/function_calls_default_args.sql
@@ -50,15 +50,72 @@ begin;
 
     create function func_with_null_defaults(
         a smallint default null,
-        b integer default null,
-        c boolean default null,
+        b int default null,
+        c bigint default null,
         d real default null,
         e double precision default null,
-        f text default null,
-        g uuid default null
+        f numeric default null,
+        g bool default null,
+        h uuid default null,
+        i text default null,
+        j date default null,
+        k time default null,
+        l time with time zone default null,
+        m timestamp default null,
+        n timestamptz default null,
+        o json default null,
+        p jsonb default null,
+        q char(2) default null,
+        r varchar(2) default null
     )
-        returns text language sql immutable
-    as $$ select a::text || b::text || c::text || d::text || e::text || f::text || g::text; $$;
+        returns text language plpgsql immutable
+    as $$
+    begin
+
+        if
+            a is null and
+            b is null and
+            c is null and
+            d is null and
+            e is null and
+            f is null and
+            g is null and
+            h is null and
+            i is null and
+            j is null and
+            k is null and
+            l is null and
+            m is null and
+            n is null and
+            o is null and
+            p is null and
+            q is null and
+            r is null
+        then
+            return 'all args null';
+        end if;
+
+        return
+            a::text || ', ' ||
+            b::text || ', ' ||
+            c::text || ', ' ||
+            d::text || ', ' ||
+            e::text || ', ' ||
+            f::text || ', ' ||
+            g::text || ', ' ||
+            h::text || ', ' ||
+            i::text || ', ' ||
+            j::text || ', ' ||
+            k::text || ', ' ||
+            l::text || ', ' ||
+            m::text || ', ' ||
+            n::text || ', ' ||
+            o::text || ', ' ||
+            p::text || ', ' ||
+            q::text || ', ' ||
+            r::text;
+    end;
+    $$;
 
     select jsonb_pretty(graphql.resolve($$
         query {
@@ -68,13 +125,26 @@ begin;
 
     select jsonb_pretty(graphql.resolve($$
         query {
-            funcWithNullDefaults(a: 1, b: 2, c: false, d: 1.1, e: 2.3, f: "f_arg", g: "8871277a-de9c-4156-b31f-5b4060001081")
-        }
-    $$));
-
-    select jsonb_pretty(graphql.resolve($$
-        query {
-            funcWithNullDefaults(a: 1, c: false, e: 2.3, g: "8871277a-de9c-4156-b31f-5b4060001081")
+            funcWithNullDefaults(
+                a: 1,
+                b: 2,
+                c: 3,
+                d: 1.1,
+                e: 2.3,
+                f: "3.4",
+                g: false,
+                h: "8871277a-de9c-4156-b31f-5b4060001081",
+                i: "text",
+                j: "2023-07-28",
+                k: "10:20",
+                l: "10:20+05:30",
+                m: "2023-07-28 12:39:05",
+                n: "2023-07-28 12:39:05+05:30",
+                o: "{\"a\": {\"b\": \"foo\"}}",
+                p: "{\"a\": {\"b\": \"foo\"}}",
+                q: "hi",
+                r: "ho"
+            )
         }
     $$));
 
