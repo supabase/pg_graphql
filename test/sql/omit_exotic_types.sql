83c5ea9a198d8fec25ac346ed1f2632967793deb
Mora <mlabisi98@gmail.com>
2024-05-15 20:20:31 -0700

    updating failing tests

--- test/sql/omit_exotic_types.sql
+++ test/sql/omit_exotic_types.sql
@@ -1,9 +1,8 @@
 begin;
 
     /*
-        Composite and array types are not currently supported as inputs
+        Composite types are not currently supported as inputs
         - confirm composites are not allowed anywhere
-        - confirm arrays are not allowed as input
     */
 
     create type complex as (r int, i int);
